3cf070991358486fed0e1924a665d2c96d339275
stuyk <trevorwessel@protonmail.com>
2025-04-18 13:32:30 -0600

    fix: config formatting

--- packages/api-main/src/config.ts
+++ packages/api-main/src/config.ts
@@ -1,19 +1,19 @@
 type Config = {
-	READ_ONLY_PORT: number,
-    WRITE_ONLY_PORT: number,
-}
+    READ_ONLY_PORT: number;
+    WRITE_ONLY_PORT: number;
+};
 
 let config: Config;
 
 export function useConfig(): Config {
-	if (typeof config !== 'undefined') {
-		return config;
-	}
+    if (typeof config !== 'undefined') {
+        return config;
+    }
 
     config = {
-		READ_ONLY_PORT: process.env.READ_ONLY_PORT ? parseInt(process.env.READ_ONLY_PORT) : 3000,
-        WRITE_ONLY_PORT: process.env.WRITE_ONLY_PORT ? parseInt(process.env.WRITE_ONLY_PORT) : 3001
+        READ_ONLY_PORT: process.env.READ_ONLY_PORT ? parseInt(process.env.READ_ONLY_PORT) : 3000,
+        WRITE_ONLY_PORT: process.env.WRITE_ONLY_PORT ? parseInt(process.env.WRITE_ONLY_PORT) : 3001,
     };
 
-	return config;
+    return config;
 }
