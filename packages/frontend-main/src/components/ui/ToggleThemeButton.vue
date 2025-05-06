7be3be2745d27cdf59caedcec8f7b415cf3f88f7
Dadidou <50441633+WaDadidou@users.noreply.github.com>
2025-05-06 19:56:16 +0200

    feat: Add dark and light mode toggle (#37)

---
+++ packages/frontend-main/src/components/ui/ToggleThemeButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useColorMode } from '@vueuse/core';
+import { Moon, Sun } from 'lucide-vue-next';
+
+import { Button } from '@/components/ui/button';
+
+const mode = useColorMode();
+</script>
+
+<template>
+  <Button @click="mode = mode === 'dark' ? 'light' : 'dark'" class="size-10">
+    <component :is="mode === 'dark' ? Moon : Sun" class="size-5" />
+  </Button>
+</template>
