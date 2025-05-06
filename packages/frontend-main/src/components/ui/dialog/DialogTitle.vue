3cdaf380d5c6ce2e357ba6389a201096e37773ee
Omar Sy <syomar.pro@gmail.com>
2025-05-06 20:28:31 +0200

    feat: Implement dialog

---
+++ packages/frontend-main/src/components/ui/dialog/DialogTitle.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { computed, type HTMLAttributes } from 'vue';
+import { DialogTitle, type DialogTitleProps, useForwardProps } from 'reka-ui';
+
+import { cn } from '@/lib/utils';
+
+const props = defineProps<DialogTitleProps & { class?: HTMLAttributes['class'] }>();
+
+const delegatedProps = computed(() => {
+    const { class: _, ...delegated } = props;
+
+    return delegated;
+});
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+  <DialogTitle
+    data-slot="dialog-title"
+    v-bind="forwardedProps"
+    :class="cn('text-lg leading-none font-semibold', props.class)"
+  >
+    <slot />
+  </DialogTitle>
+</template>
