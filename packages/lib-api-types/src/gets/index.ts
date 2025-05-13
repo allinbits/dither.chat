4b8e4f8bde025498eba0b6faeb3d19bad1a449cb
stuyk <trevorwessel@protonmail.com>
2025-05-12 21:30:03 -0600

    chore: add rest of types

--- packages/lib-api-types/src/gets/index.ts
+++ packages/lib-api-types/src/gets/index.ts
@@ -1,5 +1,6 @@
 import { t } from 'elysia';
 
+export type DislikesQuery = { hash: string; limit?: number; offset?: number; count?: boolean };
 export const DislikesQuery = t.Object({
     hash: t.String(),
     limit: t.Optional(t.Number()),
@@ -7,12 +8,14 @@ export const DislikesQuery = t.Object({
     count: t.Optional(t.Boolean()),
 });
 
+export type FeedQuery = { limit?: number; offset?: number; count?: boolean };
 export const FeedQuery = t.Object({
     limit: t.Optional(t.Number()),
     offset: t.Optional(t.Number()),
     count: t.Optional(t.Boolean()),
 });
 
+export type FlagsQuery = { hash: string; limit?: number; offset?: number; count?: boolean };
 export const FlagsQuery = t.Object({
     hash: t.String(),
     limit: t.Optional(t.Number()),
@@ -20,18 +23,21 @@ export const FlagsQuery = t.Object({
     count: t.Optional(t.Boolean()),
 });
 
+export type FollowersQuery = { limit?: number; offset?: number; address: string };
 export const FollowersQuery = t.Object({
     limit: t.Optional(t.Number()),
     offset: t.Optional(t.Number()),
     address: t.String(),
 });
 
+export type FollowingQuery = { limit?: number; offset?: number; address: string };
 export const FollowingQuery = t.Object({
     limit: t.Optional(t.Number()),
     offset: t.Optional(t.Number()),
     address: t.String(),
 });
 
+export type LikesQuery = { hash: string; limit?: number; offset?: number; count?: boolean };
 export const LikesQuery = t.Object({
     hash: t.String(),
     limit: t.Optional(t.Number()),
@@ -39,17 +45,20 @@ export const LikesQuery = t.Object({
     count: t.Optional(t.Boolean()),
 });
 
+export type PostQuery = { hash: string; post_hash?: string };
 export const PostQuery = t.Object({
     hash: t.String(),
     post_hash: t.Optional(t.String()),
 });
 
+export type PostsQuery = { limit?: number; offset?: number; address: string };
 export const PostsQuery = t.Object({
     limit: t.Optional(t.Number()),
     offset: t.Optional(t.Number()),
     address: t.String(),
 });
 
+export type RepliesQuery = { limit?: string; offset?: string; hash: string };
 export const RepliesQuery = t.Object({
     limit: t.Optional(t.String()),
     offset: t.Optional(t.String()),
