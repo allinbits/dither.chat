a1aedc37dfb7d9e4f0d7918a28e3a64eb57e43e1
WaDadidou <wadadidou@gmail.com>
2025-05-14 02:35:38 +0200

    feat: Add avatar for users

--- packages/frontend-main/src/components/ui/Feed.vue
+++ packages/frontend-main/src/components/ui/Feed.vue
@@ -2,19 +2,18 @@
 
 import { useFeed } from '@/composables/useFeed';
 
+import UserAvatarUsername from './UserAvatarUsername.vue';
+
 const { data } = useFeed();
 
 </script>
 
 <template >
-  <div class="flex flex-col w-full gap-4 border-x border-x-neutral-200 ">
-
+  <div class="flex flex-col w-full gap-4 md:border-x border-x-neutral-200">
     <div v-for="(post, index) in data" :key="index" class="flex flex-col border-b py-4 px-4">
-      <span>
+      <UserAvatarUsername :userAddress="post.author" />
+      <span class="pl-13 leading-6 text-sm">
         {{ post.message }}
-      </span>
-      <span>
-        by {{ post.author }}
       </span>
     </div>
   </div>
---
+++ packages/frontend-main/src/components/ui/UserAvatarUsername.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import UserAvatar, { type UserAvatarProps } from './UserAvatar.vue';
+
+import { cn } from '@/lib/utils';
+import { shorten } from '@/utility';
+
+defineProps<UserAvatarProps>();
+
+</script>
+
+<template>
+  <div :class="cn('flex flex-row items-center', size === 'lg' ? 'gap-4' : 'gap-3')">
+    <UserAvatar :userAddress="userAddress" :size="size"/>
+    <span :class="cn('w-[132px] font-semibold', size === 'lg' && 'text-lg')">{{ shorten(userAddress || '...............', 8, 4) }}</span>
+  </div>
+</template>
