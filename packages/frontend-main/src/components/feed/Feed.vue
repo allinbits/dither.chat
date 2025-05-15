855ad28753ef09cd566939edde60b5d18e8ec9c5
WaDadidou <wadadidou@gmail.com>
2025-05-15 15:13:20 +0200

    feat: Start Post Item UI

---
+++ packages/frontend-main/src/components/feed/Feed.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+
+import { useFeed } from '@/composables/useFeed';
+
+import PostItem from './PostItem.vue';
+
+const { data } = useFeed();
+
+</script>
+
+<template >
+  <div class="flex flex-col w-full gap-4 md:border-x border-x-neutral-200">
+    <div v-for="(post, index) in data" :key="index" class="flex flex-col border-b py-4 px-4">
+      <PostItem :post="post"/>
+    </div>
+  </div>
+</template>
---
+++ packages/frontend-main/src/components/feed/PostItem.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+
+import type { Post } from 'api-main/types/feed';
+
+import PostActions from '../feed/PostActions.vue';
+import PrettyTimestamp from '../feed/PrettyTimestamp.vue';
+import UserAvatarUsername from '../users/UserAvatarUsername.vue';
+
+defineProps<{ post: Post }>();
+
+</script>
+
+<template >
+  <UserAvatarUsername :userAddress="post.author" />
+  <span class="pl-13 leading-6 text-sm">
+    {{ post.message }}
+  </span>
+  <span>
+    by {{ post.author }}
+  </span>
+  <PrettyTimestamp :timestamp="post.timestamp"/>
+  <PostActions :post="post" :onClickLike="() => {}" :onClickDislike="() => {}" :onClickComment="() => {}"/>
+</template>
