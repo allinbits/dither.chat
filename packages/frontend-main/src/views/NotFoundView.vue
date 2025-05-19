68c49205e33c1b0bb3f3797b777aa6c257fd0763
hthieu1110 <tronghieu.ha@gmail.com>
2025-05-19 08:04:44 +0700

    feat: add NotFoundView

---
+++ packages/frontend-main/src/views/NotFoundView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import MainLayout from '@/layouts/MainLayout.vue';
+
+</script>
+
+<template>
+  <MainLayout>
+    <div class="flex flex-col items-center justify-center h-full w-full">
+      <h1 class="text-2xl font-bold">Page not found</h1>
+      <p class="text-gray-500">The page you are looking for does not exist.</p>
+    </div>
+  </MainLayout>
+</template>
