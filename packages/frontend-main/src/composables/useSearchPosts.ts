8a755a88e5e5fb956e78c1282c957caeef97204f
Yo HA <tronghieu.ha@gmail.com>
2025-05-21 18:16:34 +0700

    feat: search posts (#89)
    * wip: add search component
    
    * wip: search when typing
    
    * feat: implement search
    
    * feat: add avatar in search result + truncate text + ignore abort
    
    * fix: using vue-query for fetch posts
    
    * chore: add translations
    
    * feat: integrate with search endpoint
    
    * feat: add navigation to search results
    
    * fix: full text partial match searches
    
    ---------
    
    Co-authored-by: Hieu. Ha Trong <hieu.ha@hsc.com.vn>
    Co-authored-by: stuyk <trevorwessel@protonmail.com>

---
+++ packages/frontend-main/src/composables/useSearchPosts.ts
@@ -0,0 +1,46 @@
+import type { Post } from 'api-main/types/feed';
+
+import { ref, watch } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { refDebounced } from '@vueuse/core';
+import { useQuery } from '@tanstack/vue-query';
+
+const apiRoot = import.meta.env.VITE_API_ROOT || 'http://localhost:3000';
+
+export function useSearchPosts(minQueryLength: number = 3, debounceMs: number = 300) {
+    const { t } = useI18n();
+    const query = ref<string>('');
+    const debouncedQuery = refDebounced<string>(query, debounceMs);
+
+    const searchPosts = async ({ queryKey, signal }: { queryKey: string[]; signal: AbortSignal }) => {
+        // FIXME: We should use search endpoint instead
+        const rawResponse = await fetch(`${apiRoot}/search?text=${queryKey[1]}`, { signal });
+        const res = (await rawResponse.json()) as { status: number; rows: Post[] };
+
+        if (res.status !== 200) {
+            throw Error(t('components.SearchInput.failedToSearch'));
+        }
+
+        return res.rows;
+    };
+
+    const {
+        isLoading,
+        data: posts,
+        error,
+        refetch,
+    } = useQuery({
+        queryKey: ['searchPosts', debouncedQuery],
+        queryFn: searchPosts,
+        enabled: false,
+        retry: false,
+    });
+
+    watch(debouncedQuery, (newVal: string) => {
+        if (newVal.trim().length >= minQueryLength) {
+            refetch();
+        }
+    });
+
+    return { searchPosts, posts, isLoading, error, query };
+}
