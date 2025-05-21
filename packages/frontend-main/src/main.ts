590e42178a066e27770891c3430d1c503453153a
Yo HA <tronghieu.ha@gmail.com>
2025-05-21 18:20:01 +0700

    feat: persist wallet state (#108)
    * feat: persist wallet state
    
    * fix: fetch balance when app booted and wallet loaded from store
    
    * fix: unable to post after refresh browser
    
    ---------
    
    Co-authored-by: Hieu. Ha Trong <hieu.ha@hsc.com.vn>

--- packages/frontend-main/src/main.ts
+++ packages/frontend-main/src/main.ts
@@ -3,6 +3,8 @@ import './style.css';
 import { createApp } from 'vue';
 import { createI18n } from 'vue-i18n';
 import { VueQueryPlugin } from '@tanstack/vue-query';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 
 import Icon from './components/ui/icon/Icon.vue';
 import App from './App.vue';
@@ -10,6 +12,8 @@ import router from './router.ts';
 
 import { messages } from '@/localization';
 
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 const app = createApp(App);
 const i18n = createI18n({
     legacy: false,
@@ -18,6 +22,7 @@ const i18n = createI18n({
     messages,
 });
 app.use(i18n);
+app.use(pinia);
 app.use(router);
 app.use(VueQueryPlugin);
 app.component('Icon', Icon);
