bd3430d117534ec86748fea15163eb613eac05c5
hthieu1110 <tronghieu.ha@gmail.com>
2025-05-27 20:29:20 +0700

    feat: block scroll when dialog show

--- packages/frontend-main/src/components/ui/dialog/Dialog.vue
+++ packages/frontend-main/src/components/ui/dialog/Dialog.vue
@@ -12,17 +12,23 @@ const forwarded = useForwardPropsEmits(props, emits);
 // so we need to toggle overflow-hidden when component mounts and unmounts
 onMounted(() => {
     const html = document.documentElement;
+    const body = document.body;
     if (props.open) {
         html.classList.add('overflow-hidden');
         html.classList.add('h-full');
+        body.classList.add('overflow-hidden');
+        body.classList.add('h-full');
     }
 });
 
 onUnmounted(() => {
     const html = document.documentElement;
+    const body = document.body;
     if (props.open) {
         html.classList.remove('overflow-hidden');
         html.classList.remove('h-full');
+        body.classList.remove('overflow-hidden');
+        body.classList.remove('h-full');
     }
 });
 
