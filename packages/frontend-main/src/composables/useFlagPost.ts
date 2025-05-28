d7b72cd2089bd39611bd1d57128aea3152c5bdd6
WaDadidou <wadadidou@gmail.com>
2025-05-28 18:54:25 +0200

    chore: Add TODO

--- packages/frontend-main/src/composables/useFlagPost.ts
+++ packages/frontend-main/src/composables/useFlagPost.ts
@@ -1,5 +1,5 @@
 import { ref } from 'vue';
-import { useMutation, useQueryClient } from '@tanstack/vue-query';
+import { useMutation } from '@tanstack/vue-query';
 
 import { useWallet } from './useWallet';
 
@@ -10,7 +10,6 @@ interface FlagPostRequestMutation {
 
 export function useFlagPost(
 ) {
-    const queryClient = useQueryClient();
     const wallet = useWallet();
     const txError = ref<string>();
     const txSuccess = ref<string>();
@@ -31,53 +30,7 @@ export function useFlagPost(
                 return txSuccess.value;
             }
         },
-        onMutate: async (variables) => {
-            // const postOpts = post({ hash: variables.hash, postHash: variables.postHash });
-            // const repliesOpts = replies({ hash: variables.hash });
-
-            // await queryClient.cancelQueries(postOpts);
-            // await queryClient.cancelQueries(repliesOpts);
-
-            // const previousPost = queryClient.getQueryData(
-            //     postOpts.queryKey,
-            // ) as Post | undefined;
-            // const previousReplies = queryClient.getQueryData(
-            //     repliesOpts.queryKey,
-            // ) as InfiniteData<Post[], unknown> | undefined;
-
-            // return { previousReplies, previousPost };
-        },
-        onSuccess: (hash, variables, context) => {
-            // if (!hash) throw new Error('Error: No hash in TX');
-
-            // const postOpts = post({ hash: variables.hash, postHash: variables.postHash });
-            // const repliesOpts = replies({ hash: variables.hash });
-
-            // const optimisticNewPost = context.previousPost ? { ...context.previousPost, replies: (context.previousPost.replies || 0) + 1 } : undefined;
-            // const optimisticNewReply = buildNewPost(variables.message, variables.photonValue, hash);
-
-            // const newPages = context.previousReplies?.pages ? [...context.previousReplies.pages] : [];
-            // if (newPages.length > 0) {
-            //     newPages[0] = [optimisticNewReply, ...newPages[0]];
-            // }
-            // else {
-            //     newPages.push([optimisticNewReply]);
-            // }
-            // const newRepliesData: InfiniteData<Post[], unknown> = {
-            //     pages: newPages,
-            //     pageParams: context.previousReplies?.pageParams ?? [0],
-            // };
-
-            // queryClient.setQueryData(postOpts.queryKey, optimisticNewPost);
-            // queryClient.setQueryData(repliesOpts.queryKey, newRepliesData);
-        },
-        onError: (_, variables, context) => {
-            // const postOpts = post({ hash: variables.hash, postHash: variables.postHash });
-            // const repliesOpts = replies({ hash: variables.hash });
-
-            // queryClient.setQueryData(postOpts.queryKey, context?.previousPost);
-            // queryClient.setQueryData(repliesOpts.queryKey, context?.previousReplies);
-        },
+        // TODO: onMutate, onSuccess, onError
     });
 
     return {
