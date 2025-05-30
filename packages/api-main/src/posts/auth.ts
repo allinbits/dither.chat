1a202ae936d12f84b9b311d4291eb7cd7a1ef4f7
stuyk <trevorwessel@protonmail.com>
2025-05-30 09:39:06 -0600

    feat: define max age for cookie

--- packages/api-main/src/posts/auth.ts
+++ packages/api-main/src/posts/auth.ts
@@ -4,17 +4,17 @@ import { type Posts } from '@atomone/dither-api-types';
 
 import { useUserAuth } from '../shared/useUserAuth';
 
-const { verify } = useUserAuth();
+const { verifyAndCreate } = useUserAuth();
 
 export async function Auth(body: typeof Posts.AuthBody.static, auth: Cookie<string | undefined>) {
     try {
         if (Object.hasOwn(body, 'json')) {
-            return verify(body.pub_key.value, body.signature, body.id);
+            return verifyAndCreate(body.pub_key.value, body.signature, body.id);
         }
 
-        const result = verify(body.pub_key.value, body.signature, body.id);
+        const result = verifyAndCreate(body.pub_key.value, body.signature, body.id);
         if (result.status === 200) {
-            auth.set({ sameSite: 'strict', httpOnly: false, value: result.bearer });
+            auth.set({ sameSite: 'strict', httpOnly: false, value: result.bearer, maxAge: 259200 });
             return { status: 200 };
         }
     }
