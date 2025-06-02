f9acd902d76c96f0b574761a7a085af30729ad0c
stuyk <trevorwessel@protonmail.com>
2025-06-02 08:45:00 -0600

    chore: add test helper

--- packages/api-main/tests/v1.test.ts
+++ packages/api-main/tests/v1.test.ts
@@ -998,10 +998,14 @@ describe('get post from followed', async () => {
                 deleted_at: Date;
                 deleted_reason: string;
                 deleted_hash: string;
+                quantity: string;
             }[];
         }>(`following-posts?address=${walletA.publicKey}`, 'READ');
         assert.isOk(readResponse?.status === 200, `response was not okay, got ${readResponse?.status}`);
         assert.lengthOf(readResponse.rows, 0);
+        assert.isOk(readResponse.rows[0].author, 'Author was not included');
+        assert.isOk(readResponse.rows[0].message, 'message was not included');
+        assert.isOk(readResponse.rows[0].quantity, 'quantity was not included');
     });
 
     it('POST - now followed user posts', async () => {
