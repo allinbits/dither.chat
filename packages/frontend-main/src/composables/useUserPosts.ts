bd49b2a7177e8a0fd29de520c3db18987cb93d0d
WaDadidou <wadadidou@gmail.com>
2025-06-03 22:21:07 +0200

    fix: Plug photon filter to other posts lists

--- packages/frontend-main/src/composables/useUserPosts.ts
+++ packages/frontend-main/src/composables/useUserPosts.ts
@@ -1,7 +1,11 @@
 import type { Post } from 'api-main/types/feed';
 
 import { type Ref } from 'vue';
+import { refDebounced } from '@vueuse/core';
 import { infiniteQueryOptions, useInfiniteQuery } from '@tanstack/vue-query';
+import { storeToRefs } from 'pinia';
+
+import { useFiltersStore } from '@/stores/useFiltersStore';
 
 const apiRoot = import.meta.env.VITE_API_ROOT ?? 'http://localhost:3000';
 const LIMIT = 15;
@@ -10,11 +14,13 @@ interface Params {
     userAddress: Ref<string>;
 }
 
-export const userPosts = (params: Params) =>
-    infiniteQueryOptions({
-        queryKey: ['posts', params.userAddress],
+export const userPosts = (params: Params) => {
+    const { minSendAmount } = storeToRefs(useFiltersStore());
+    const debouncedMinSendAmount = refDebounced<number>(minSendAmount, 600);
+    return infiniteQueryOptions({
+        queryKey: ['posts', params.userAddress, debouncedMinSendAmount],
         queryFn: async ({ pageParam = 0 }) => {
-            const res = await fetch(`${apiRoot}/posts?address=${params.userAddress.value}&offset=${pageParam}&limit=${LIMIT}`);
+            const res = await fetch(`${apiRoot}/posts?address=${params.userAddress.value}&offset=${pageParam}&limit=${LIMIT}&minQuantity=${Math.trunc(debouncedMinSendAmount.value)}`);
             const json = await res.json() as { status: number; rows: Post[] };
             return json.rows ?? [];
         },
@@ -25,6 +31,7 @@ export const userPosts = (params: Params) =>
         },
         enabled: () => !!params.userAddress.value,
     });
+};
 
 export function useUserPosts(params: Params) {
     return useInfiniteQuery(userPosts(params));
