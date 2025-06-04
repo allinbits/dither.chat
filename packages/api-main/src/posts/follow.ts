32eeefd02b03d57d4ac92d2615e502afbd68b63b
stuyk <trevorwessel@protonmail.com>
2025-06-04 12:38:37 -0600

    fix: setting hash for onConflictDoUpdate

--- packages/api-main/src/posts/follow.ts
+++ packages/api-main/src/posts/follow.ts
@@ -1,25 +1,8 @@
 import { type Posts } from '@atomone/dither-api-types';
-import { sql } from 'drizzle-orm';
 
 import { getDatabase } from '../../drizzle/db';
 import { FollowsTable } from '../../drizzle/schema';
 import { notify } from '../shared/notify';
-
-const statementAddFollower = getDatabase()
-    .insert(FollowsTable)
-    .values({
-        follower: sql.placeholder('follower'),
-        following: sql.placeholder('following'),
-        hash: sql.placeholder('hash'),
-        timestamp: sql.placeholder('timestamp'),
-    })
-    .onConflictDoUpdate({
-        target: [FollowsTable.follower, FollowsTable.following],
-        set: {
-            removed_at: null,
-        },
-    })
-    .prepare('stmnt_add_follower');
 
 export async function Follow(body: typeof Posts.FollowBody.static) {
     if (body.hash.length !== 64) {
@@ -39,12 +22,21 @@ export async function Follow(body: typeof Posts.FollowBody.static) {
     }
 
     try {
-        const result = await statementAddFollower.execute({
-            follower: body.from.toLowerCase(),
-            following: body.address.toLowerCase(),
-            hash: body.hash.toLowerCase(),
-            timestamp: new Date(body.timestamp),
-        });
+        const result = await getDatabase()
+            .insert(FollowsTable)
+            .values({
+                follower: body.from.toLowerCase(),
+                following: body.address.toLowerCase(),
+                hash: body.hash.toLowerCase(),
+                timestamp: new Date(body.timestamp),
+            })
+            .onConflictDoUpdate({
+                target: [FollowsTable.follower, FollowsTable.following],
+                set: {
+                    removed_at: null,
+                    hash: body.hash.toLowerCase(),
+                },
+            }).execute();
 
         if (typeof result.rowCount !== 'number' || result.rowCount <= 0) {
             return { status: 400, error: 'failed to add follow, follow already exists' };
