633b12c453fd5b831936121c37f67a5b1e638a03
Dadidou <50441633+WaDadidou@users.noreply.github.com>
2025-06-05 19:56:23 +0200

    fix: Optimistic update for post's replies count (#191)
    * fix: Optimistic update for post's replies count
    
    * fix: Update user posts after replying, rename variables
    
    * fix: useCreateReplies smol fix

--- packages/frontend-main/src/utility/optimisticBuilders.ts
+++ packages/frontend-main/src/utility/optimisticBuilders.ts
@@ -1,11 +1,11 @@
 import type { InfiniteData } from '@tanstack/vue-query';
 import type { Post } from 'api-main/types/feed';
 
-interface BuildNewPostParams {
+interface NewPostParams {
     message: string; hash: string; postHash: string | null; author: string; quantity: number;
 }
 // Returns a new Post, used for display
-export const buildNewPost = ({ message, hash, postHash, author, quantity }: BuildNewPostParams): Post => ({
+export const newPost = ({ message, hash, postHash, author, quantity }: NewPostParams): Post => ({
     hash: hash.toLowerCase(),
     post_hash: postHash ? postHash.toLocaleLowerCase() : null,
     author,
@@ -24,12 +24,12 @@ export const buildNewPost = ({ message, hash, postHash, author, quantity }: Buil
     removed_by: null,
 });
 
-interface BuildNewInfiniteDataParams<T> {
+interface InfiniteDataWithNewItemParams<T> {
     previousItems: InfiniteData<T[], unknown> | undefined;
     newItem: T;
 }
 // Returns an InfiniteData with pages and pageParams after adding a new item to previous items, used for display
-export function buildNewInfiniteData<T>({ previousItems, newItem }: BuildNewInfiniteDataParams<T>) {
+export function infiniteDataWithNewItem<T>({ previousItems, newItem }: InfiniteDataWithNewItemParams<T>) {
     const newPages = previousItems?.pages ? [...previousItems.pages] : [];
     if (newPages.length > 0) {
         newPages[0] = [newItem, ...newPages[0]];
@@ -42,4 +42,24 @@ export function buildNewInfiniteData<T>({ previousItems, newItem }: BuildNewInfi
         pageParams: previousItems?.pageParams ?? [0],
     };
     return newData;
+}
+
+interface InfiniteDataWithUpdatedPostParams {
+    previousPosts: InfiniteData<Post[], unknown> | undefined;
+    updatedPost: Post;
+}
+// Returns an InfiniteData with pages and pageParams after updating a Post, used for display
+export function infiniteDataWithUpdatedPost({ previousPosts, updatedPost }: InfiniteDataWithUpdatedPostParams) {
+    const pages = previousPosts?.pages.map(page =>
+        page.map(post =>
+            post.hash === updatedPost.hash
+                ? updatedPost
+                : post,
+        ),
+    ) || [];
+    const postsData: InfiniteData<Post[]> = {
+        pageParams: previousPosts?.pageParams || [],
+        pages,
+    };
+    return postsData;
 }
