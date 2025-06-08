11e18886eedd2ccee500478440c00f930a6104ab
Miguel Victoria <miguelvicvil@gmail.com>
2025-06-08 15:49:50 +0200

    feat: allow searching by post owner

--- packages/api-main/tests/v1.test.ts
+++ packages/api-main/tests/v1.test.ts
@@ -771,6 +771,27 @@ describe('v1 - mod', { sequential: true }, () => {
         assert.isOk(results2?.status === 200);
         assert.isOk(results2.rows.length <= 0);
     });
+
+    it('Search - /search post with owner', async () => {
+        const body: typeof Posts.PostBody.static = {
+            from: addressUserA,
+            hash: getRandomHash(),
+            msg: 'content not related at all with owner',
+            quantity: '1',
+            timestamp: '2025-04-16T19:46:42Z',
+        };
+
+        const response = await post(`post`, body);
+        assert.isOk(response?.status === 200, 'response was not okay');
+
+        const results1 = await get<{ status: number; rows: { message: string }[]; users: string[] }>(
+            `search?text=${addressUserA}`,
+        );
+        assert.isOk(results1?.status === 200);
+        assert.isOk(results1.rows.length > 1);
+        assert.isOk(results1.users.length === 1);
+        assert.isOk(results1.users[0] === addressUserA);
+    });
 });
 
 describe('v1/auth', async () => {
