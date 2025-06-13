5d2a93a8b311b6b15422a1ed7375696a491979cd
hthieu1110 <tronghieu.ha@gmail.com>
2025-06-13 11:08:08 +0700

    fix lint

--- packages/frontend-main/src/views/EnvConfigView.vue
+++ packages/frontend-main/src/views/EnvConfigView.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { watch } from 'vue';
 import { toast } from 'vue-sonner';
 import { debouncedWatch } from '@vueuse/core';
 import JsonEditorVue from 'json-editor-vue';
