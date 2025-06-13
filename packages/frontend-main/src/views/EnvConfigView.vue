9fb2cba5d395c8acd651091d3ff4ec99da89e916
Yo HA <tronghieu.ha@gmail.com>
2025-06-13 22:25:54 +0700

    Update packages/frontend-main/src/views/EnvConfigView.vue
    Co-authored-by: Dadidou <50441633+WaDadidou@users.noreply.github.com>

--- packages/frontend-main/src/views/EnvConfigView.vue
+++ packages/frontend-main/src/views/EnvConfigView.vue
@@ -22,7 +22,7 @@ debouncedWatch(configStore.config, () => {
 
 <template>
   <MainLayout>
-    <div class="flex flex-col">
+    <div class="flex flex-col mb-2">
       <div class="flex flex-row items-center border-b py-2 pr-4">
         <RouterLink to="/settings" class="w-32">
           <Button size="sm" class="w-full text-left" variant="link">
