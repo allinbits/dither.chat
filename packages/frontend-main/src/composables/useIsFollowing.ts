8eb1a74bca99bfcae88a941148aad5fe8dc5eb12
Dadidou <50441633+WaDadidou@users.noreply.github.com>
2025-06-16 00:43:33 +0200

    fix: copy to clipboard text (#213)
    * fix: copy to clipboard text
    
    * fix: Copy full post URL to clipboard

--- packages/frontend-main/src/composables/useIsFollowing.ts
+++ packages/frontend-main/src/composables/useIsFollowing.ts
@@ -31,7 +31,7 @@ export const isFollowing = (params: Params) => {
             return true;
         },
         enabled: () => !!params.followerAddress.value && !!params.followingAddress.value,
-        retry: false,
+        staleTime: Infinity,
     });
 };
 
