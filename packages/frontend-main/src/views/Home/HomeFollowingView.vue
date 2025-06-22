420363f78688877bcb2ba17777d19062b8a215c1
WaDadidou <wadadidou@gmail.com>
2025-06-22 16:51:51 +0200

    fix: Handle routes for tabs, add Views

---
+++ packages/frontend-main/src/views/Home/HomeFollowingView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+import HomeViewWrapper from './HomeViewWrapper.vue';
+
+import UserFollowing from '@/components/users/UserFollowing.vue';
+
+</script>
+
+<template>
+  <HomeViewWrapper>
+    <UserFollowing/>
+  </HomeViewWrapper>
+</template>
