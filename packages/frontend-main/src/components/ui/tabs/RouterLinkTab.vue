48400b41944ea26448b4180dce1b3d46a7b00a54
WaDadidou <wadadidou@gmail.com>
2025-06-23 18:01:24 +0200

    fix: After merge

--- packages/frontend-main/src/components/ui/tabs/RouterLinkTab.vue
+++ packages/frontend-main/src/components/ui/tabs/RouterLinkTab.vue
@@ -8,9 +8,9 @@ defineProps<{ label: string; isActive: boolean; to: string }>();
 </script>
 
 <template>
-  <!-- <RouterLink class="max-w-[180px] w-full" :to="to"> -->
-  <RouterLink :class="cn(isActive && 'border-b-4 border-primary', 'max-w-[180px] w-full h-full pb-2 text-center')" :to="to">
-    <h2 class="text-lg font-semibold">{{ label }}</h2>
+  <RouterLink class="flex-1 h-[40x] py-6 hover:bg-accent active:bg-accent" :to="to">
+    <div :class="cn('mx-auto w-full max-w-[180px] h-full pb-2 text-center', isActive && 'border-b-4 border-primary')">
+      <h2 class="text-lg font-semibold">{{ label }}</h2>
+    </div>
   </RouterLink>
-  <!-- </RouterLink> -->
 </template>
