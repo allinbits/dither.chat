e103ff724c0403d02326567c7deab005ca08a17f
Dadidou <50441633+WaDadidou@users.noreply.github.com>
2025-06-26 18:12:53 +0200

    fix: Show tokens amount instead of count for likes/dislikes/flags (#280)

--- packages/frontend-main/src/components/posts/PostActions.vue
+++ packages/frontend-main/src/components/posts/PostActions.vue
@@ -12,8 +12,8 @@ import { cn } from '@/utility';
 import { formatAmount, formatCompactNumber } from '@/utility/text';
 
 defineProps<{ post: Post }>();
-const isXs = useMediaQuery('(max-width: 414px)');
-const buttonWrapperClass = 'flex-1 min-w-[68px]';
+const isXs = useMediaQuery('(max-width: 422px)');
+const buttonWrapperClass = 'flex-1 min-w-[70px]';
 const buttonClass = 'flex flex-row items-center gap-1 p-2 rounded-full hover:bg-accent active:bg-accent transition-colors';
 const buttonLabelClass = 'text-[#A2A2A9] text-xs font-medium';
 
@@ -35,7 +35,7 @@ function handleAction(type: keyof PopupState, post: Post) {
 </script>
 
 <template>
-  <div :class="cn('flex flex-row items-center justify-between gap-y-1 pr-2', isXs && 'flex-wrap pb-2')">
+  <div :class="cn('flex flex-row items-center justify-between pr-2', isXs && 'flex-wrap pb-2')">
     <div :class="buttonWrapperClass">
       <button :class="buttonClass" @click.stop="handleAction('reply', post)">
         <MessageCircle class="size-5" color="#A2A2A9" />
@@ -46,25 +46,26 @@ function handleAction(type: keyof PopupState, post: Post) {
     <div :class="buttonWrapperClass">
       <button :class="buttonClass" @click.stop="handleAction('like', post)">
         <ThumbsUp class="size-5" color="#A2A2A9" />
-        <span :class="buttonLabelClass">{{ formatCompactNumber(post.likes) }}</span>
+        <span :class="buttonLabelClass">{{ formatCompactNumber(post.likes_burnt) }}</span>
       </button>
     </div>
 
     <div :class="buttonWrapperClass">
       <button :class="buttonClass" @click.stop="handleAction('dislike', post)">
         <ThumbsDown class="size-5 scale-x-[-1]" color="#A2A2A9" />
-        <span :class="buttonLabelClass">{{ formatCompactNumber(post.dislikes) }}</span>
+        <span :class="buttonLabelClass">{{ formatCompactNumber(post.dislikes_burnt) }}</span>
       </button>
     </div>
 
     <div :class="buttonWrapperClass">
       <button :class="buttonClass" @click.stop="handleAction('flag', post)">
         <Flag class="size-5" color="#A2A2A9" />
+        <span :class="buttonLabelClass">{{ formatCompactNumber(post.flags_burnt) }}</span>
       </button>
     </div>
 
-    <div class="ml-auto text-xs text-right  text-neutral-400">
-      <span class="w-[64px]">{{ formatAmount(post.quantity, 6) + ' ' }}</span>
+    <div class="flex flex-wrap justify-end gap-x-1 ml-auto text-xs text-right  text-neutral-400">
+      <span class="w-[64px]">{{ formatAmount(post.quantity, 6) }}</span>
       <span>PHOTON</span>
     </div>
   </div>
