5e1e5d17f6fb6a7b5dcfb466ec1477270d846243
Dadidou <50441633+WaDadidou@users.noreply.github.com>
2025-07-08 17:58:30 +0200

    feat: Back button and nav scroll behavior (#311)
    * feat: Add a back button everywhere + page title
    
    * fix: Enhance scroll positioning when navigate + Add routes names as variables

---
+++ packages/frontend-main/src/views/ViewHeading.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+import { useRouter } from 'vue-router';
+import { ArrowLeft } from 'lucide-vue-next';
+
+defineProps<{ title?: string }>();
+
+const { back } = useRouter();
+
+</script>
+
+<template>
+  <div class="flex flex-row items-center w-full border-b px-2 h-[62px]">
+    <button @click="back" class="flex flex-row items-center gap-1 p-2 mr-5 rounded-full hover:bg-accent active:bg-accent transition-colors">
+      <ArrowLeft class="size-6" />
+    </button>
+    <h1 v-if="title" class="text-lg font-semibold ">
+      {{ title }}
+    </h1>
+  </div>
+</template>
