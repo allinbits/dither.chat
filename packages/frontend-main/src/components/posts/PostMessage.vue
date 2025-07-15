c929a3b690baffd091fba1d48c9c548e68a68b57
WaDadidou <wadadidou@gmail.com>
2025-07-15 22:27:05 +0200

    feat: Show youtube videos in post message

--- packages/frontend-main/src/components/posts/PostMessage.vue
+++ packages/frontend-main/src/components/posts/PostMessage.vue
@@ -1,9 +1,34 @@
 <script lang="ts" setup>
-defineProps<{ message: string }>();
+import { computed } from 'vue';
+import DOMPurify from 'dompurify';
+
+import { purifyHtml } from '@/utility/sanitize';
+
+const props = defineProps<{ message: string }>();
+
+const youtubeRegex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})/g;
+
+const parsedMessage = computed(() => {
+    const rawHtml = props.message.replace(
+        youtubeRegex,
+        (_, videoId) => `
+        <iframe
+          class="w-full aspect-video rounded-sm"
+          src="https://www.youtube.com/embed/${videoId}"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+    `,
+    );
+    console.log('DOMPurify.sanitize(rawHtml)DOMPurify.sanitize(rawHtml)', DOMPurify.sanitize(rawHtml));
+    return purifyHtml(rawHtml);
+});
 </script>
 
 <template>
-  <span class="leading-6 text-sm break-word">
-    {{ message }}
-  </span>
+  <div
+    class="leading-6 text-sm"
+    v-html="parsedMessage"
+  />
 </template>
