251327404d2f26965827d984a9c5ccfc6c5fddb2
mazzy89 <salvatore@newtendermint.org>
2025-10-21 20:44:59 +0200

    chore: add attribute

--- packages/api-main/src/observability.ts
+++ packages/api-main/src/observability.ts
@@ -2,11 +2,15 @@ import type { Config } from './config';
 
 import { opentelemetry } from '@elysiajs/opentelemetry';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { resourceFromAttributes } from '@opentelemetry/resources';
 import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-node';
 
 export function observability(config: Config) {
     return opentelemetry({
         serviceName: process.env.FLY_APP_NAME || 'api-main',
+        resource: resourceFromAttributes({
+            'service.group': 'diter.chat',
+        }),
         spanProcessors: [
             new BatchSpanProcessor(
                 new OTLPTraceExporter({
