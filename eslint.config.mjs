aad5552a6e0a7501e1407e937635f6caca19d902
Lucio Rubens <luciorubeens@users.noreply.github.com>
2025-10-22 14:57:28 -0300

    antfu

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,123 +1,23 @@
-import { builtinModules } from 'module';
+import antfu from '@antfu/eslint-config';
 
-import jsLint from '@eslint/js';
-import stylistic from '@stylistic/eslint-plugin';
-import pluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
-import vueLint from 'eslint-plugin-vue';
-import globals from 'globals';
-import tsLint from 'typescript-eslint';
-
-export default [
-    // config parsers
-    {
-        files: ['**/*.{js,mjs,cjs,ts,mts,jsx,tsx}'],
-        languageOptions: {
-            parser: tsLint.parser,
-            parserOptions: {
-                sourceType: 'module',
-            },
-        },
+export default antfu(
+  {
+    typescript: true,
+    vue: true,
+    jsonc: true,
+    yaml: true,
+    markdown: true,
+    formatters: true,
+    stylistic: {
+      indent: 2,
+      quotes: 'single',
+      semi: true,
     },
-    {
-        files: ['*.vue', '**/*.vue'],
-        languageOptions: {
-            parser: vueLint.parser,
-            parserOptions: {
-                parser: '@typescript-eslint/parser',
-                sourceType: 'module',
-            },
-        },
+  },
+  {
+    files: ['tsconfig.json', 'package.json'],
+    rules: {
+      'jsonc/sort-keys': 'off',
     },
-    // config envs
-    {
-        languageOptions: {
-            globals: { ...globals.browser, ...globals.node },
-        },
-    },
-    // rules
-    jsLint.configs.recommended,
-    ...tsLint.configs.recommended,
-    ...vueLint.configs['flat/essential'],
-    {
-        rules: {
-            '@typescript-eslint/consistent-type-imports': [
-                'error',
-                {
-                    prefer: 'type-imports',
-                    fixStyle: 'separate-type-imports',
-                },
-            ],
-            '@typescript-eslint/no-explicit-any': [
-                'warn',
-                { ignoreRestArgs: true },
-            ],
-            '@typescript-eslint/no-unused-vars': [
-                'error',
-                {
-                    args: 'all',
-                    argsIgnorePattern: '^_',
-                    caughtErrors: 'all',
-                    caughtErrorsIgnorePattern: '^_',
-                    destructuredArrayIgnorePattern: '^_',
-                    varsIgnorePattern: '^_',
-                },
-            ],
-        },
-    },
-    {
-        plugins: {
-            'vue': vueLint,
-            '@typescript-eslint': tsLint.plugin,
-            'simple-import-sort': pluginSimpleImportSort,
-        },
-        rules: {
-            'simple-import-sort/imports': [
-                'error',
-                {
-                    groups: [
-                        [
-                            `node:`,
-                            `^(${builtinModules.join('|')})(/|$)`,
-                        ],
-                        // style less,scss,css
-                        ['^.+\\.less$', '^.+\\.s?css$'],
-                        // Side effect imports.
-                        ['^\\u0000'],
-                        ['^@?\\w.*\\u0000$', '^[^.].*\\u0000$', '^\\..*\\u0000$'],
-                        ['^vue', '^@vue', '^@?\\w', '^\\u0000'],
-                        ['^@/utils'],
-                        ['^@/composables'],
-                        // Parent imports. Put `..` last.
-                        ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
-                        // Other relative imports. Put same-folder imports and `.` last.
-                        ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
-                    ],
-                },
-            ],
-            // see more vue rules: https://eslint.vuejs.org/rules/
-            'vue/no-mutating-props': 'error',
-            'vue/multi-word-component-names': 'off',
-            'vue/html-indent': ['error', 2],
-        },
-    },
-    // see: https://eslint.style/guide/getting-started
-    // see: https://github.com/eslint-stylistic/eslint-stylistic/blob/main/packages/eslint-plugin/configs/disable-legacy.ts
-    stylistic.configs['disable-legacy'],
-    stylistic.configs.customize({
-        indent: 4,
-        quotes: 'single',
-        semi: true,
-        jsx: true,
-    }),
-    {
-    // https://eslint.org/docs/latest/use/configure/ignore
-        ignores: [
-            'node_modules',
-            '**/node_modules',
-            '**/*.conf',
-            '**/nginx/**',
-            '**/data/postgres_data',
-            '**/dist/**',
-        ],
-    },
-];
+  },
+);
