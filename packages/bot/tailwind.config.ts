8daff4f46b11042958438d8d74a402ce76773505
Alan R. Soares <alanrsoars@gmail.com>
2025-10-23 16:21:09 +1300

    chore: remove broken import

--- packages/bot/tailwind.config.ts
+++ packages/bot/tailwind.config.ts
@@ -1,11 +1,7 @@
 import type { Config } from "tailwindcss";
 
-import { breakpoints } from "./src/utility/breakpoints";
-
 export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    screens: breakpoints,
-  },
+  theme: {},
 } satisfies Config;
