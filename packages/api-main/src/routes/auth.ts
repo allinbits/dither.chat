d8fdc8e808976023a0587338091a1dcf1d41d5b9
Lucio Rubens <luciorubeens@users.noreply.github.com>
2025-10-24 07:56:46 -0300

    fix: lint

--- packages/api-main/src/routes/auth.ts
+++ packages/api-main/src/routes/auth.ts
@@ -8,14 +8,14 @@ import * as PostRequests from '../posts/index';
  * Authentication-related routes
  */
 export const authRoutes = new Elysia()
-    .get('/auth-verify', ({ cookie: { auth } }) => GetRequests.AuthVerify(auth))
-    .post('/auth-create', ({ body, request }) => PostRequests.AuthCreate(body, request), { body: Posts.AuthCreateBody })
-    .post('/auth', ({ body, cookie: { auth }, request }) => PostRequests.Auth(body, auth, request), {
-        body: t.Object({
-            id: t.Number(),
-            pub_key: t.Object({ type: t.String(), value: t.String() }),
-            signature: t.String(),
-            json: t.Optional(t.Boolean()),
-        }),
-    })
-    .post('/logout', ({ cookie: { auth } }) => PostRequests.Logout(auth));
+  .get('/auth-verify', ({ cookie: { auth } }) => GetRequests.AuthVerify(auth))
+  .post('/auth-create', ({ body, request }) => PostRequests.AuthCreate(body, request), { body: Posts.AuthCreateBody })
+  .post('/auth', ({ body, cookie: { auth }, request }) => PostRequests.Auth(body, auth, request), {
+    body: t.Object({
+      id: t.Number(),
+      pub_key: t.Object({ type: t.String(), value: t.String() }),
+      signature: t.String(),
+      json: t.Optional(t.Boolean()),
+    }),
+  })
+  .post('/logout', ({ cookie: { auth } }) => PostRequests.Logout(auth));
