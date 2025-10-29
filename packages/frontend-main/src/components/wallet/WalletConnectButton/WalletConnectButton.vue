7dc9489cd23287ed878d0d4c73a79c158149380b
Alan R. Soares <alanrsoars@gmail.com>
2025-10-29 18:22:45 +1300

    feat(wip): useWalletConnect

--- packages/frontend-main/src/components/wallet/WalletConnectButton/WalletConnectButton.vue
+++ packages/frontend-main/src/components/wallet/WalletConnectButton/WalletConnectButton.vue
@@ -8,6 +8,7 @@ import {
 } from '@/components/ui/popover';
 import UserAvatarUsername from '@/components/users/UserAvatarUsername.vue';
 import { useWallet } from '@/composables/useWallet';
+import { useWalletConnect } from '@/composables/useWalletConnect';
 import { useWalletDialogStore } from '@/stores/useWalletDialogStore';
 
 import WalletConnectPopoverContent from './WalletConnectPopoverContent.vue';
@@ -17,6 +18,7 @@ const isError = ref(false);
 
 const { address, loggedIn } = useWallet();
 const walletDialogStore = useWalletDialogStore();
+const wallectConnectStore = useWalletConnect();
 
 const connectedState = computed(() => !isConnecting.value && loggedIn.value && !isError.value);
 </script>
@@ -37,6 +39,9 @@ const connectedState = computed(() => !isConnecting.value && loggedIn.value && !
     <template v-else>
       <Button class="w-[207px]" @click="walletDialogStore.showDialog">
         {{ $t('components.WalletConnect.button') }}
+      </Button>
+      <Button class="w-[207px]" @click="wallectConnectStore.connect">
+        Open WalletConnect
       </Button>
     </template>
   </div>
