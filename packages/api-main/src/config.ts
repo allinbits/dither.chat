45fceef27efe43a59b4f83887a25f4d6ce74e503
Alan Soares <alanrsoars@gmail.com>
2025-11-18 07:54:37 +1300

    fix: enforce strong secrets for AUTH and JWT in configuration (#471)
    * fix: enforce strong secrets for AUTH and JWT in configuration
    
    Updated the configuration logic to throw errors if the AUTH and JWT environment variables are not set or are using default values. This ensures that strong secrets are provided for secure application operation.
    
    * fix: add missing JWT to api-main and AUTH to reader-main docker-compose

--- packages/api-main/src/config.ts
+++ packages/api-main/src/config.ts
@@ -1,4 +1,3 @@
-import crypto from 'node:crypto';
 import process from 'node:process';
 
 import dotenv from 'dotenv';
@@ -33,8 +32,8 @@ export function useConfig(): Config {
   }
 
   if (!process.env.JWT) {
-    console.log(`JWT was not set, defaulting to a randomized byte hex string.`);
-    process.env.JWT = crypto.randomBytes(128).toString('hex');
+    console.error(`Failed to specify JWT, no JWT secret provided`);
+    throw new Error(`JWT must be set to a strong secret`);
   }
 
   if (typeof process.env.JWT_STRICTNESS === 'undefined') {
@@ -50,8 +49,8 @@ export function useConfig(): Config {
   config = {
     PORT: process.env.PORT ? Number.parseInt(process.env.PORT) : 3000,
     PG_URI: process.env.PG_URI,
-    AUTH: process.env.AUTH ?? 'default',
-    JWT: process.env.JWT ?? 'default-secret-key',
+    AUTH: process.env.AUTH as string,
+    JWT: process.env.JWT as string,
     JWT_STRICTNESS: process.env.JWT_STRICTNESS as JWT_STRICTNESS,
     DISCORD_WEBHOOK_URL: process.env.DISCORD_WEBHOOK_URL,
   };
