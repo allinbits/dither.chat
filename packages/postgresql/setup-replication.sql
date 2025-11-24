b44d4e2003010759fd99b3915811d253fe229bd8
Alan Soares <alanrsoars@gmail.com>
2025-11-24 23:42:32 +1300

    fix: simplify Dockerfiles for api-main and reader-main (#490)
    * refactor: simplify Dockerfiles for api-main and reader-main
    
    * Removed multi-stage build setup and unnecessary workspace copying.
    * Updated working directory to /app for consistency.
    
    * fix: re-add NODE_TLS_REJECT_UNAUTHORIZED
    
    * refactor: reduce docker images by copying only required code into stage 2
    
    * chore: remove PostgreSQL replication setup SQL file
    
    Replication subscriber is now initialized when first run by the feed
    sync app.
    
    Also removing the SQL file fixes an issue that made PostgreSQl docker
    initialization fail.
    
    * fix: change Docker files to use `--frozen-lockfile`
    
    Done to have reproducible builds, otherwise we could have unexpected
    issues with dependencies.
    
    ---------
    
    Co-authored-by: jeronimoalbi <894299+jeronimoalbi@users.noreply.github.com>

--- packages/postgresql/setup-replication.sql
+++
@@ -1,5 +0,0 @@
-/* Create a logical replication slot for the feed sync service */
-SELECT pg_create_logical_replication_slot('slot1', 'pgoutput');
-
-/* Create a publication to replicate feed table insert operations */
-CREATE PUBLICATION feed_pub FOR TABLE feed WITH (publish = 'insert');
